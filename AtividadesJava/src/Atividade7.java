e1e998d7ef652e98a1799532ef370758775240f1
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-28 21:49:18 -0300

    Atividade 7

---
+++ AtividadesJava/src/Atividade7.java
@@ -0,0 +1,14 @@
+public class Atividade7 {
+    public static void main(String[] args) {
+        int a = 0, b = 0,soma = 0;
+
+        a = 10;
+        b = 5;
+        soma = (a + b);
+
+        System.out.println("A soma de " + a + " e " + b + " é: " + soma);
+
+
+
+    }
+}
