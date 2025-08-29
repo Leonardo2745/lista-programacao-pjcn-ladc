ce1b37c5f08a55e4edd805135aa560f2515b718a
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-28 21:41:07 -0300

    Atividade 6

---
+++ AtividadesJava/src/Atividade6.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Atividade6 {
+    public static void main(String[] args) {
+        int idade = 20;
+        final int MESES = 12;
+
+        System.out.println("Idade: " + idade +
+                "\nIdade em meses: "+(idade*MESES));
+    }
+}
