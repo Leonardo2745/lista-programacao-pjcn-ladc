9639a16f535758b8f69ab9997dbef498c4205c69
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-28 21:33:52 -0300

    Atividade 5

---
+++ AtividadesJava/src/Atividade5.java
@@ -0,0 +1,10 @@
+public class Atividade5 {
+    public static void main(String[] args) {
+        String curso = "Java e C#";
+        int cargaHoraria = 360;
+
+        System.out.println("Curso: " + curso +
+                "\nCarga horária: " + cargaHoraria + "hrs");
+
+    }
+}
