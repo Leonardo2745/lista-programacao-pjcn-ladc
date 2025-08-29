9ff28b5e05e394b7c2a6db8e814831a624acf5b8
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-29 11:48:31 -0300

    Correção atividade 7

--- AtividadesJava/src/Atividade7.java
+++ AtividadesJava/src/Atividade7.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Atividade7 {
     public static void main(String[] args) {
         int a = 0, b = 0,soma = 0;
+        Scanner leia = new Scanner(System.in);
 
-        a = 10;
-        b = 5;
+        System.out.println("Digite o primeiro número: ");
+        a = leia.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        b = leia.nextInt();
+
         soma = (a + b);
 
         System.out.println("A soma de " + a + " e " + b + " é: " + soma);
