b6afeb07b023c87d47d5ef3a97078f8819138b9a
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-28 21:08:55 -0300

    Atividade 2

---
+++ AtividadesJava/src/Atividade2.java
@@ -0,0 +1,7 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+        int dia = 27, mes = 04, ano = 2005;
+
+        System.out.println("Dia: " + dia + "\nMês: " + mes + "\nAno: " + ano);
+    }
+}
