0aa899d555abd536fd07080b202030424b19f13f
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-28 21:17:24 -0300

    Atividade 3

---
+++ AtividadesJava/src/Atividade3.java
@@ -0,0 +1,13 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+        int qtdAlunos = 30;
+        double mediaTurma = 25;
+        boolean aprovada = true;
+        String professor = "Victor";
+
+        System.out.println("Número de alunos na turma: " + qtdAlunos +
+                "\nMedia da turma: " + mediaTurma +
+                "\nSituação da turma: " + aprovada +
+                "\nProfessor da turma: " + professor);
+    }
+}
