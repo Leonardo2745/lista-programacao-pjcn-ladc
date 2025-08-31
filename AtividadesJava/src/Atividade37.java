bc6213338c7e026e707f65c5d7cc970bf013ba47
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-08-31 16:47:51 -0300

    Atividade 37

---
+++ AtividadesJava/src/Atividade37.java
@@ -0,0 +1,11 @@
+public class Atividade37 {
+    public static void main(String[] args) {
+
+        System.out.println("Números de 1 até 10: ");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
